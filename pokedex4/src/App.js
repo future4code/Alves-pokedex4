1b99ec88bb6d4f9d693a6cf27d816ff39e486396
Laksmin-Monteiro-Bittencourt <lakshmi96@yahoo.com.br>
2022-07-20 17:59:47 -0300

    requisição

--- pokedex4/src/App.js
+++ pokedex4/src/App.js
@@ -1,9 +1,11 @@
-import { Router } from "./Routes/Router";
+// import { Router } from "./Routes/Router";
+import Detalhes from "./Pages/Detalhes";
 
 export default function App() {
   return (
     <div>
-      <Router />
+      {/* <Router /> */}
+      <Detalhes/>
     </div>
   );
 }
