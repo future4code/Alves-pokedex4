3295d25563e1cbc37cf8710580e141fd19a53e45
Laksmin-Monteiro-Bittencourt <lakshmi96@yahoo.com.br>
2022-07-20 20:23:29 -0300

    estilização inacabada

--- pokedex4/src/App.js
+++ pokedex4/src/App.js
@@ -1,11 +1,21 @@
 // import { Router } from "./Routes/Router";
 import Detalhes from "./Pages/Detalhes";
+import styled from "styled-components";
+
+const Div = styled.div`
+  background-color: grey;
+  width: 1340px;
+  height: 750px;
+  margin: 0px;
+  
+
+`
 
 export default function App() {
   return (
-    <div>
+    <Div>
       {/* <Router /> */}
       <Detalhes/>
-    </div>
+    </Div>
   );
 }
